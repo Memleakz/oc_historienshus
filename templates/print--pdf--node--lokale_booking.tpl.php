0ee2f67395ccc99683b630b8f20d4147158e13fc
Tobias F. Petersen <tobsf@hotmail.com>
2016-03-17 11:18:10 +0100

    First Commit

---
+++ templates/print--pdf--node--lokale_booking.tpl.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ * This template handles setting up the correct templates for the loakl historisk
+ * hus. it handles FREE and Non FREE receipts.
+ */
+$wrapper = entity_metadata_wrapper('node', $node);
+/*
+ * Setup start_time objects
+ */
+$start_date = date('d/m/Y',$wrapper->field_start_dato->value());
+$start_time = date("H:i",  $wrapper->field_start_tidspunkt->value());
+/*
+ * Setup stop_time objects
+ */
+$slut_dato = $wrapper->field_slut_dato->value();
+$slut_dato = date('d/m/Y H:m',$slut_dato);
+$slut_time = date("H:i", $wrapper->field_slut_tidspunkt->value());
+
+$Addresse_data = $wrapper->field_adresse->value();
+
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PDF Kvittering</title>
+</head>
+<body>
+<div style="width: 100%;">
+	<div style="float:left;width:50%;padding-top: 150px;">
+		<b>
+		<?php echo $wrapper->field_fornavn->value() . " " . $wrapper->field_efternavn->value(); ?><br/>
+		<?php echo $Addresse_data['thoroughfare'] ?><br/>
+		<?php echo $Addresse_data['postal_code'] . " " . $Addresse_data['locality']  ?><br/>
+                </b>
+	</div>
+	<div style="float:right;width:50%;text-align: right;">
+	<img style="height:100px;" src="http://historienshus.dk/~/media/Spots/Subsites/Historiens%20Hus/stadsarkivet_logo215x106%20copy.ashx"><br/><br/>
+	<b>By- og Kulturforvaltningen<br/>
+	Odense Kommune<br/><br/>
+	Klosterbakken 2<br/>
+	5000 Odense C<br/>
+	Tlf 6551 4427</b>
+	</div>
+	<div style="float:left;width:75%;">
+        <p><?php echo date("j F, Y") ?></p>
+	<p><?php echo "VEDR." . $wrapper->field_sted->value()->name . ", Klosterbakken 2" ?></p>
+	<p>Hermed bekræftes, at Mødelokalet er reserveret til jer <?php echo $start_date  ?>,
+		  kl.<?php echo $start_time ?>-<?php echo $slut_time ?></p>
+    <p>Som det vil være bekendt, har der pågået drøftelser i Odense Kommune
+	   vedrørende indførelse af brugerbetaling for benyttelsen af lokalerne på
+	   Klosterbakken 2.</p>
+	<p>
+		Dette arbejde er nu bragt til ende med beslutning om at der fremover skal
+		betales leje for brugen af lokalerne af Klosterbakken 2. Lejen dækker
+		udgifter til lys, varme samt slutrengøring af lokalet.
+	</p>
+	<p>
+		Det bemærkes, at arrangementer, der afholdes med baggrund i
+		Folkeoplysnings-loven, er gratis.
+	</p>
+	<p>
+		Satserne fremgår af vedlagte Retningslinier for brug/leje af Klosterbakken
+		2, idet retningslinierne samtidig med fastlægger de øvrige
+		betingelser/regler for brug af Klosterbakken 2.
+	</p>
+	<p>
+		På den baggrund fremsendes tillige kontrakt for Deres leje af ovennævnte
+		lokale. Såfremt De ønsker at benytte lokalet som ovenfor anført, bedes De
+		underskrive kontrakten og returnere den hertil<?php
+                if($wrapper->field_aktuel_leje->value() == 0)
+                {
+                    echo ".";
+                }
+                else
+                {
+                    echo ", samtidig med der inden 14
+                            dage fra dato indbetales kr. 50 i kontanter eller med check.";
+                }
+                ?>
+	</p>
+	<p>
+		For arrangementer under Folkeoplysningsloven bedes ud over den
+		underskrevne kontrakt fremsendt dokumentation for tilskud/tilsagn om
+		tilskud fra Odense Kommune.
+	</p>
+	<p>
+		Med venlig hilsen
+	</p>
+	<p>
+		Historiens Hus<br/>
+		ibu@odense.dk
+	</p>
+        </div>
+	<!-- 
+	Page2
+	-->
+        <div style="width: 100%;">
+            	<div style="float:left;width:50%;padding-top: 150px;">
+		<b>
+		<?php echo $wrapper->field_fornavn->value() . " " . $wrapper->field_efternavn->value(); ?><br/>
+		<?php echo $Addresse_data['thoroughfare'] ?><br/>
+		<?php echo $Addresse_data['postal_code'] . " " . $Addresse_data['locality']  ?><br/>
+                </b>
+                </div>
+                <div style="float:right;width:50%;text-align: right;">
+                <img style="height:100px;" src="http://historienshus.dk/~/media/Spots/Subsites/Historiens%20Hus/stadsarkivet_logo215x106%20copy.ashx"><br/><br/>
+                <b>By- og Kulturforvaltningen<br/>
+                Odense Kommune<br/><br/>
+                Klosterbakken 2<br/>
+                5000 Odense C<br/>
+                Tlf 6551 4427</b>
+                </div>
+        <div style="float:left;width:100%;padding-top: 25px;text-align:center;">
+            <p>NØGLEKVITTERING</p>
+        </div>
+        <div style="float:left;width:100%;">
+            <p>For modtagelse af Nøgle nr._____________________ </p>
+            <p>i forbindelse med lån af Mødelokalet den. <?php echo $start_date  ?> kvitteres hermed:</p>
+            <p><span style="padding-right:5em">Dato:____________</span><span>Underskrift:___________________________</span></p><br/>
+            <hr>
+            <p>Nøglen er modtaget retur den._______________</p>
+            <p>Bemærkninger:</p><br/><br/><br/><br/>
+            <p>Hvis der er forhold, der ikke svare til det forventede, eller hvis der er forslag<br/>til forandringer og forbedringer , høre vi gerne nærmere </p>
+            <p>
+		Med venlig hilsen
+            </p>
+            <p>Ingrid Bundgaard</p>
+            <p>
+                    Historiens Hus<br/>
+                    Klosterbakken 2<br/>
+                    5000 Odense C<br/>
+            </p>
+        </div>
+	</div>
+</div>
+</body>
+</html>
